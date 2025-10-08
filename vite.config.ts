39affc067acb6335b2e6018f8b5393766596e04c
JenishCloudEngineer <jenishjk98@gmail.com>
2025-10-08 18:48:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolioclaws/', // 👈 Add this line — your GitHub repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
